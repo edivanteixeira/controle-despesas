f0ddc94e48fa8e8e5164bdaf4fa4ed863c700a44
Guilherme André Rebello <guilherme.rebello@efrete.com.br>
2022-12-21 17:11:07 -0300

    First commit: tests

--- WeatherForecast.cs
+++ WeatherForecast.cs
@@ -5,6 +5,7 @@ public class WeatherForecast
     public DateTime Date { get; set; }
 
     public int TemperatureC { get; set; }
+    public int MyProperty { get; set; }
 
     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
